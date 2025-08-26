cb6df58419cff77b14981c6ec3a9d9fd13fa32f6
Rowan LeBlanc <role7830@colorado.edu>
2025-08-26 14:10:22 -0600

    Update problem1.m

--- problem1.m
+++ problem1.m
@@ -3,7 +3,26 @@
 % File name: Problem1
 % Created: 9/8/23
 
-function dxdt = problem1(t,xvar)
+init_cond = [1;1;1;1];
+
+t_span = [0;20];
+
+[t, sol] = ode45(@problem1a, t_span, init_cond);
+
+figure
+labels = {'w (n.d.)','x (n.d.)','y (n.d.)','z (n.d.)'};
+for i = 1:4
+    subplot(4,1,i)
+    plot(t, sol(:,i),'LineWidth',1.5)
+    ylabel(labels{i})
+    if i==4
+        xlabel('Time (n.d.)')
+    end
+    grid on
+end
+sgtitle('Problem 1a: Evolution of w, x, y, z')
+
+function dxdt = problem1a(t,xvar)
     w=xvar(1);
     x=xvar(2);
     y=xvar(3);
@@ -15,4 +34,3 @@ function dxdt = problem1(t,xvar)
     dxdt(3)=(2.*w)-x-(2.*z);
     dxdt(4)=(x.*y)-(y.^2)-(3.*z.^3);
 end
-
